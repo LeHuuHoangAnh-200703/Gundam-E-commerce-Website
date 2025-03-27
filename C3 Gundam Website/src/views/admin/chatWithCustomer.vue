b31c73e4cdaa57f67d64aa6ffdd3a28ff486e8b5
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-03-27 20:47:15 +0700

    fix chat

--- C3 Gundam Website/src/views/admin/chatWithCustomer.vue
+++ C3 Gundam Website/src/views/admin/chatWithCustomer.vue
@@ -133,125 +133,134 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="relative bg-[#F2F2F7] w-full min-h-screen font-sans">
-        <div class="flex gap-3">
-            <SideBar />
-            <div class="relative p-4 flex flex-col gap-4 w-full overflow-auto">
-                <Navbar />
-                <div class="flex lg:flex-row flex-col gap-4 justify-center lg:justify-between items-center">
-                    <h1 class="font-bold text-[20px] uppercase">Tin nhắn từ khách hàng</h1>
-                </div>
-                <div class="w-full flex lg:flex-row flex-col gap-8 items-start">
-                    <div
-                        class="flex flex-col gap-5 bg-white p-4 rounded-lg shadow-lg w-full min-h-[calc(100vh-25vh)] overflow-hidden lg:w-[40%]">
-                        <div class="flex flex-col gap-2">
-                            <p class="font-semibold text-[20px]">Đoạn chat</p>
-                            <div class="relative flex justify-center flex-1 gap-2 max-w-xl">
-                                <input type="text"
-                                    class="items-center w-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
-                                    placeholder="Tìm kiếm tên khách hàng ..." />
-                                <i
-                                    class="fa-solid fa-magnifying-glass absolute top-[10px] right-3 text-[22px] text-[#003171]"></i>
-                            </div>
-                            <div class="flex flex-col gap-2 mt-3 overflow-y-auto max-h-[calc(100vh-300px)]">
-                                <div v-for="room in chatRooms" :key="room.roomCode"
-                                    class="flex gap-2 items-center hover:bg-gray-200 p-2 cursor-pointer border-b-2 pb-4"
-                                    @click="selectRoom(room)">
-                                    <div class="flex gap-2 items-center">
-                                        <img :src="room.senderAvatar || '../../assets/img/avatar.jpg'"
-                                            class="w-[50px] h-[50px] rounded-full" alt="" />
-                                        <div class="flex flex-col justify-center">
-                                            <p class="text-[14px] font-bold">{{ room.senderName }}</p>
-                                            <div class="whitespace-nowrap text-ellipsis overflow-hidden w-56">
-                                                <p
-                                                    class="overflow-hidden text-ellipsis whitespace-nowrap text-gray-500 text-[12px]">
-                                                    {{ room.messages.length > 0 ? room.messages[room.messages.length -
-                                                        1].text : "Chưa có tin nhắn" }}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <span v-if="unreadMessagesCount[room.roomCode] > 0"
-                                        class="bg-red-500 text-white text-[12px] font-bold rounded-full w-5 h-5 flex items-center justify-center">
-                                        {{ unreadMessagesCount[room.roomCode] }}
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="flex flex-col gap-8 w-full h-full overflow-hidden">
-                        <div v-if="selectedRoom"
-                            class="bg-white p-4 w-full h-full border-2 rounded-lg shadow-lg flex flex-col gap-4 overflow-hidden">
-                            <div class="flex gap-2 items-center">
-                                <img :src="selectedRoom.senderAvatar || '../../assets/img/avatar.jpg'"
-                                    class="w-[50px] h-[50px] rounded-full" alt="" />
-                                <p class="text-[14px] font-bold">{{ selectedRoom.senderName }}</p>
-                            </div>
-                            <hr />
-                            <div class="flex flex-col gap-4 flex-grow overflow-y-auto max-h-[calc(100vh-53vh)]">
-                                <div v-for="(msg, index) in messages" :key="index">
-                                    <div v-if="msg.role === 'admin'" class="flex flex-col justify-end gap-2 items-end">
-                                        <div class="bg-[#4169E1] p-2 rounded-t-lg rounded-l-lg">
-                                            <p class="text-white text-[14px] inline-block">{{ msg.TinNhan }}</p>
-                                        </div>
-                                        <small class="text-[#333] text-[10px] self-end">{{ msg.ThoiGian }}</small>
-                                    </div>
-                                    <div v-if="msg.role === 'user'" class="flex gap-2">
-                                        <img :src="selectedRoom.senderAvatar || '../../assets/img/avatar.jpg'"
-                                            class="w-[35px] h-[35px] rounded-full" alt="" />
-                                        <div class="flex flex-col gap-1">
-                                            <div class="bg-gray-200 p-2 rounded-md self-end">
-                                                <p class="text-[#333] text-[14px] inline-block">{{ msg.TinNhan }}</p>
-                                            </div>
-                                            <small class="text-[#333] text-[10px]">{{ msg.ThoiGian }}</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr />
-                            <form @submit.prevent="sendMessage">
-                                <div class="flex justify-between w-full gap-4 items-end">
-                                    <input v-model="message" type="text"
-                                        class="items-center w-full h-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
-                                        placeholder="Tin nhắn của bạn ..." />
-                                    <button type="submit"
-                                        class="flex justify-center items-center bg-[#4169E1] px-5 py-4 text-white rounded-md">
-                                        <i class="fa-solid fa-arrow-up text-[16px] font-bold"></i>
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                        <div v-else
-                            class="bg-white p-4 w-full border-2 rounded-lg shadow-lg flex items-center justify-center h-full">
-                            <div class="flex justify-center items-center m-auto w-full h-full">
-                                <div class="flex flex-col items-center justify-center gap-3">
-                                    <p class="font-semibold text-[18px] lg:text-[24px] text-center">
-                                        Vui lòng chọn một khách hàng để trò chuyện!
-                                    </p>
-                                    <img src="https://res.cloudinary.com/dwcajbc6f/image/upload/v1739607250/cute-astronaut-sleeping-pillow-illustration_m4shij.png"
-                                        class="w-[200px] lg:w-[350px]" alt="">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  <div class="relative bg-[#F2F2F7] w-full min-h-screen font-sans">
+    <div class="flex gap-3">
+      <SideBar />
+      <div class="relative p-4 flex flex-col gap-4 w-full overflow-auto">
+        <Navbar />
+        <div class="flex lg:flex-row flex-col gap-4 justify-center lg:justify-between items-center">
+          <h1 class="font-bold text-[20px] uppercase">Tin nhắn từ khách hàng</h1>
         </div>
+        <div class="w-full flex lg:flex-row flex-col gap-8 items-start">
+          <div
+            class="flex flex-col gap-5 bg-white p-4 rounded-lg shadow-lg w-full min-h-[calc(100vh-25vh)] overflow-hidden lg:w-[40%]">
+            <div class="flex flex-col gap-2">
+              <p class="font-semibold text-[20px]">Đoạn chat</p>
+              <div class="relative flex justify-center flex-1 gap-2 max-w-xl">
+                <input type="text"
+                  class="items-center w-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
+                  placeholder="Tìm kiếm tên khách hàng ..." />
+                <i class="fa-solid fa-magnifying-glass absolute top-[10px] right-3 text-[22px] text-[#003171]"></i>
+              </div>
+              <div class="flex flex-col gap-2 mt-3 overflow-y-auto max-h-[calc(100vh-300px)]">
+                <div v-for="room in chatRooms" :key="room.roomCode"
+                  class="flex gap-2 items-center hover:bg-gray-200 p-2 cursor-pointer border-b-2 pb-4"
+                  @click="selectRoom(room)">
+                  <div class="flex gap-2 items-center">
+                    <img :src="room.senderAvatar || '../../assets/img/avatar.jpg'"
+                      class="w-[50px] h-[50px] rounded-full" alt="" />
+                    <div class="flex flex-col justify-center">
+                      <p class="text-[14px] font-bold">{{ room.senderName }}</p>
+                      <div class="whitespace-nowrap text-ellipsis overflow-hidden w-56">
+                        <p class="overflow-hidden text-ellipsis whitespace-nowrap text-gray-500 text-[12px]">
+                          {{ room.messages.length > 0 ? room.messages[room.messages.length -
+                            1].text : "Chưa có tin nhắn" }}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <span v-if="unreadMessagesCount[room.roomCode] > 0"
+                    class="bg-red-500 text-white text-[12px] font-bold rounded-full w-5 h-5 flex items-center justify-center">
+                    {{ unreadMessagesCount[room.roomCode] }}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="flex flex-col gap-8 w-full h-full overflow-hidden">
+            <div v-if="selectedRoom"
+              class="bg-white p-4 w-full h-full border-2 rounded-lg shadow-lg flex flex-col gap-4 overflow-hidden">
+              <div class="flex gap-2 items-center">
+                <img :src="selectedRoom.senderAvatar || '../../assets/img/avatar.jpg'"
+                  class="w-[50px] h-[50px] rounded-full" alt="" />
+                <p class="text-[14px] font-bold">{{ selectedRoom.senderName }}</p>
+              </div>
+              <hr />
+              <div :class="(messages.length > 0) ? '' : 'items-center justify-center'" class="flex flex-col gap-4 flex-grow overflow-y-auto max-h-[calc(100vh-53vh)]">
+                <div v-if="messages.length > 0">
+                  <div v-for="(msg, index) in messages" :key="index">
+                    <div v-if="msg.role === 'admin'" class="flex flex-col justify-end gap-2 items-end">
+                      <div class="bg-[#4169E1] p-2 rounded-t-lg rounded-l-lg">
+                        <p class="text-white text-[14px] inline-block">{{ msg.TinNhan }}</p>
+                      </div>
+                      <small class="text-[#333] text-[10px] self-end">{{ msg.ThoiGian }}</small>
+                    </div>
+                    <div v-if="msg.role === 'user'" class="flex gap-2">
+                      <img :src="selectedRoom.senderAvatar || '../../assets/img/avatar.jpg'"
+                        class="w-[35px] h-[35px] rounded-full" alt="" />
+                      <div class="flex flex-col gap-1">
+                        <div class="bg-gray-200 p-2 rounded-md self-end">
+                          <p class="text-[#333] text-[14px] inline-block">{{ msg.TinNhan }}</p>
+                        </div>
+                        <small class="text-[#333] text-[10px]">{{ msg.ThoiGian }}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div v-else class="flex flex-col items-center justify-center">
+                  <p class="text-[20px] font-semibold text-gray-600">Hiện tại không có tin nhắn!</p>
+                  <img src="https://res.cloudinary.com/dwcajbc6f/image/upload/v1739607250/cute-astronaut-sleeping-pillow-illustration_m4shij.png" class="w-[100px] h-[100px]" alt="">
+                </div>
+              </div>
+              <hr />
+              <form @submit.prevent="sendMessage">
+                <div class="flex justify-between w-full gap-4 items-end">
+                  <input v-model="message" type="text"
+                    class="items-center w-full h-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
+                    placeholder="Tin nhắn của bạn ..." />
+                  <label for="image_upload">
+                    <i class="fa-solid fa-image text-gray-500 text-[35px] cursor-pointer"></i>
+                    <input type="file" class="hidden" id="image_upload">
+                  </label>
+                  <button type="submit"
+                    class="flex justify-center items-center bg-[#4169E1] px-5 py-4 text-white rounded-md">
+                    <i class="fa-solid fa-arrow-up text-[16px] font-bold"></i>
+                  </button>
+                </div>
+              </form>
+            </div>
+            <div v-else
+              class="bg-white p-4 w-full border-2 rounded-lg shadow-lg flex items-center justify-center h-full">
+              <div class="flex justify-center items-center m-auto w-full h-full">
+                <div class="flex flex-col items-center justify-center gap-3">
+                  <p class="font-semibold text-[18px] lg:text-[24px] text-center">
+                    Vui lòng chọn một khách hàng để trò chuyện!
+                  </p>
+                  <img
+                    src="https://res.cloudinary.com/dwcajbc6f/image/upload/v1739607250/cute-astronaut-sleeping-pillow-illustration_m4shij.png"
+                    class="w-[200px] lg:w-[350px]" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 .active-link {
-    background: #DB3F4C;
-    color: white;
+  background: #DB3F4C;
+  color: white;
 }
 
 .fixed {
-    transform: translateX(100%);
-    transition: transform 0.3s ease-in-out;
+  transform: translateX(100%);
+  transition: transform 0.3s ease-in-out;
 }
 
 .fixed.translate-x-0 {
-    transform: translateX(0);
+  transform: translateX(0);
 }
 </style>
--- C3 Gundam Website/src/views/client/ChatBox.vue
+++ C3 Gundam Website/src/views/client/ChatBox.vue
@@ -4,7 +4,6 @@ import Header from "@/components/client/Header.vue";
 import Footer from "@/components/client/Footer.vue";
 import BackToTop from "@/components/client/BackToTop.vue";
 import { io } from "socket.io-client";
-import axios from "axios";
 
 // Khai báo các biến reactive
 const messages = ref([]);
@@ -19,6 +18,19 @@ const socket = io("http://localhost:3000", {
     userName: userName.value,
   },
 });
+
+const escapeHtml = (unsafe) => {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+};
+
+const handleFileUpload = (event) => {
+    formData.value.images = Array.from(event.target.files);
+};
 
 // Theo dõi thay đổi trong localStorage
 watch(
@@ -37,6 +49,9 @@ watch(
 
 // Gửi tin nhắn
 const sendMessage = async () => {
+  if (message.value) {
+    message.value = escapeHtml(message.value);
+  }
   if (message.value.trim()) {
     const messageData = {
       roomCode: roomCode.value,
@@ -121,76 +136,78 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="bg-[#1A1D27] relative overflow-hidden min-h-screen font-sans scroll-smooth flex flex-col">
-        <Header />
-        <div class="relative mb-5 lg:mx-[200px] flex justify-center items-center flex-grow">
-            <div class="w-full m-4">
-                <h1 class="font-bold text-white text-[20px] my-4">
-                    Đoạn chat <i class="fa-solid fa-comments"></i>
-                </h1>
-                <div
-                    class="bg-[#242424] flex flex-col min-h-[calc(100vh-30vh)] w-full gap-3 overflow-hidden px-6 py-3 rounded [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]">
-                    <div class="flex gap-3 items-center text-white">
-                        <img src="../../assets/img/avatar.jpg"
-                            class="w-[50px] h-[50px] rounded-full [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]"
-                            alt="" />
-                        <div class="flex flex-col">
-                            <p class="font-semibold text-[18px]">C3 GUNDAM STORE</p>
-                            <p class="text-[12px]">Hoạt động từ (7:30-22:30)</p>
-                        </div>
-                    </div>
-                    <hr />
-                    <div class="flex flex-col gap-4 flex-grow overflow-y-auto max-h-[calc(100vh-40vh)]">
-                        <div v-for="(msg, index) in messages" :key="index">
-                            <div class="flex flex-col gap-1" :class="{ 'items-end': msg.role === 'user' }"
-                                v-if="msg.role === 'user'">
-                                <div class="bg-[#4169E1] p-2 rounded-t-lg rounded-l-lg self-end">
-                                    <p class="text-white text-[14px] inline-block">
-                                        {{ msg.TinNhan }}
-                                    </p>
-                                </div>
-                                <small class="text-white text-[10px]">{{ msg.ThoiGian }}</small>
-                            </div>
-                            <div v-if="msg.role === 'admin'" class="flex gap-2">
-                                <img src="../../assets/img/avatar.jpg" class="w-[35px] h-[35px] rounded-full" alt="" />
-                                <div class="flex flex-col gap-1">
-                                    <div class="p-2 rounded-md bg-gray-200 text-[#333]">
-                                        <p class="text-[14px]">{{ msg.TinNhan }}</p>
-                                    </div>
-                                    <small class="text-white text-[10px]">{{ msg.ThoiGian }}</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <hr class="my-3" />
-                    <form @submit.prevent="sendMessage">
-                        <div class="flex justify-between w-full gap-4 items-center">
-                            <input type="text" v-model="message"
-                                class="items-center w-full h-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
-                                placeholder="Tin nhắn của bạn ..." />
-                            <button type="submit"
-                                class="flex justify-center items-center bg-[#4169E1] px-5 py-4 text-white rounded-md">
-                                <i class="fa-solid fa-arrow-up text-[16px] font-bold"></i>
-                            </button>
-                        </div>
-                    </form>
-                </div>
+  <div class="bg-[#1A1D27] relative overflow-hidden min-h-screen font-sans scroll-smooth flex flex-col">
+    <Header />
+    <div class="relative mb-5 lg:mx-[200px] flex justify-center items-center flex-grow">
+      <div class="w-full m-4">
+        <h1 class="font-bold text-white text-[20px] my-4">
+          Đoạn chat <i class="fa-solid fa-comments"></i>
+        </h1>
+        <div
+          class="bg-[#242424] flex flex-col min-h-[calc(100vh-30vh)] w-full gap-3 overflow-hidden px-6 py-3 rounded [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]">
+          <div class="flex gap-3 items-center text-white">
+            <img src="../../assets/img/avatar.jpg"
+              class="w-[50px] h-[50px] rounded-full [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]" alt="" />
+            <div class="flex flex-col">
+              <p class="font-semibold text-[18px]">C3 GUNDAM STORE</p>
+              <p class="text-[12px]">Hoạt động từ (7:30-22:30)</p>
             </div>
+          </div>
+          <hr />
+          <div class="flex flex-col gap-4 flex-grow overflow-y-auto max-h-[calc(100vh-40vh)]">
+            <div v-for="(msg, index) in messages" :key="index">
+              <div class="flex flex-col gap-1" :class="{ 'items-end': msg.role === 'user' }" v-if="msg.role === 'user'">
+                <div class="bg-[#4169E1] p-2 rounded-t-lg rounded-l-lg self-end">
+                  <p class="text-white text-[14px] inline-block">
+                    {{ msg.TinNhan }}
+                  </p>
+                </div>
+                <small class="text-white text-[10px]">{{ msg.ThoiGian }}</small>
+              </div>
+              <div v-if="msg.role === 'admin'" class="flex gap-2">
+                <img src="../../assets/img/avatar.jpg" class="w-[35px] h-[35px] rounded-full" alt="" />
+                <div class="flex flex-col gap-1">
+                  <div class="p-2 rounded-md bg-gray-200 text-[#333]">
+                    <p class="text-[14px]">{{ msg.TinNhan }}</p>
+                  </div>
+                  <small class="text-white text-[10px]">{{ msg.ThoiGian }}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr class="my-3" />
+          <form @submit.prevent="sendMessage">
+            <div class="flex justify-between w-full gap-4 items-center">
+              <input type="text" v-model="message"
+                class="items-center w-full h-full p-3 bg-gray-200 text-[12px] shadow font-semibold tracking-wider text-black rounded-md focus:outline-none"
+                placeholder="Tin nhắn của bạn ..." />
+              <label for="image_upload">
+                <i class="fa-solid fa-image text-white text-[35px] cursor-pointer"></i>
+                <input type="file" class="hidden" id="image_upload" multiple @change="handleFileUpload">
+              </label>
+              <button type="submit"
+                class="flex justify-center items-center bg-[#4169E1] px-5 py-4 text-white rounded-md">
+                <i class="fa-solid fa-arrow-up text-[16px] font-bold"></i>
+              </button>
+            </div>
+          </form>
         </div>
-        <Footer />
-        <BackToTop />
+      </div>
     </div>
+    <Footer />
+    <BackToTop />
+  </div>
 </template>
 
 <style scoped>
 .slide-fade-enter-active,
 .slide-fade-leave-active {
-    transition: all 0.5s ease;
+  transition: all 0.5s ease;
 }
 
 .slide-fade-enter,
 .slide-fade-leave-to {
-    transform: translateX(100%);
-    opacity: 0;
+  transform: translateX(100%);
+  opacity: 0;
 }
 </style>
