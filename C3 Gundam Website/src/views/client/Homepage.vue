bdcdcd26df1f33ff82bd6e43e6cc28d36897e15e
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-04-22 08:53:29 +0700

    sap xep san pham

--- C3 Gundam Website/src/views/client/Homepage.vue
+++ C3 Gundam Website/src/views/client/Homepage.vue
@@ -30,6 +30,21 @@ const listChoices = [
     },
 ]
 
+const arrange = ref([
+    {
+        name: "Bán chạy nhất",
+        type: "BestSeller"
+    },
+    {
+        name: "Giá thấp đến cao",
+        type: "LowPrice"
+    },
+    {
+        name: "Giá cao đến thấp",
+        type: "HighPrice"
+    }
+])
+
 const listProducts = ref([]);
 const searchQuery = ref("");
 const selectedType = ref("All");
@@ -45,14 +60,6 @@ const showNotification = (msg, type) => {
         notification.value.message = '';
     }, 3000);
 };
-const selectTypeProducts = (type) => {
-    selectedType.value = type;
-    // Kiểm tra nếu trang hiện tại vượt quá tổng số trang sau khi lọc, đưa về trang gần nhất
-    if (currentPage.value > totalPages.value) {
-        currentPage.value = totalPages.value;
-    }
-};
-
 
 const handleSearch = (query) => {
     searchQuery.value = query;
@@ -67,7 +74,7 @@ const fectchProducts = async () => {
                 NgayBan: new Date(product.NgayBan)
             }
         })
-        
+
         const productsNormal = listProducts.value.filter(product => product.TrangThai !== 'Ngừng kinh doanh');
         const productsSpecial = listProducts.value.filter(product => product.TrangThai === 'Ngừng kinh doanh');
         listProducts.value = [...productsNormal.sort((a, b) => b.NgayBan - a.NgayBan), ...productsSpecial];
@@ -99,11 +106,31 @@ const addToCart = async (idProDuct) => {
     }, 3000);
 }
 
+const selectTypeProducts = (type) => {
+    selectedType.value = type;
+    // Kiểm tra nếu trang hiện tại vượt quá tổng số trang sau khi lọc, đưa về trang gần nhất
+    if (currentPage.value > totalPages.value) {
+        currentPage.value = totalPages.value;
+    }
+};
+
 const filteredProducts = computed(() => {
-    return listProducts.value.filter(product => {
+    const products = listProducts.value;
+    if (selectedType.value === "BestSeller") {
+        return products.slice().sort((a,b) => b.LuotBan - a.LuotBan);
+    }
+
+    if (selectedType.value === "LowPrice") {
+        return products.slice().sort((a,b) => Number(a.GiaBan) - Number(b.GiaBan));
+    }
+
+    if (selectedType.value === "HighPrice") {
+        return products.slice().sort((a,b) => Number(b.GiaBan) - Number(a.GiaBan));
+    }
+
+    return products.filter(product => {
         const matchesType = selectedType.value === "All" || product.LoaiSanPham === selectedType.value;
-        const matchesSearch = product.TenSanPham.toLowerCase().includes(searchQuery.value.toLowerCase());
-        return matchesType && matchesSearch;
+        return matchesType;
     });
 });
 
@@ -157,43 +184,58 @@ onMounted(() => {
                 </div>
             </div>
         </div>
-        <div
-            class="my-2 bg-gradient-to-b text-center lg:text-start from-[rgba(219,63,76,0.7)] to-[#1A1D27] p-5 lg:mx-[210px]">
-            <p class="text-white font-semibold uppercase text-[22px] lg:text-[26px]">Vũ trụ Gundam thu nhỏ trong từng mô
-                hình, chỉ có tại C3 GUNDAM.</p>
-            <p class="text-white font-medium my-1">Tại đây bạn có thể chọn được những mẫu Gundam yêu thích chính hãng từ
-                BANDAI NAMCO.</p>
-        </div>
-        <div class="my-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 m-5 lg:mx-[210px]">
-            <button v-for="(choice, index) in listChoices" :key="index" @click.prevent="selectTypeProducts(choice.type)"
-                class="w-full bg-gradient-to-b text-center flex flex-col gap-2 lg:text-start from-[rgba(219,63,76,0.7)] to-[#1A1D27] p-4">
-                <p class="text-[18px] font-semibold text-white">{{ choice.name }}</p>
-                <span class="lg:w-[100px] w-full h-[2px] bg-white"></span>
-                <p class="text-[14px] text-white font-medium">> Hiển thị tất cả</p>
-            </button>
-        </div>
-        <div class="mt-10 mb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-5 lg:mx-[210px]">
-            <div v-for="(product, index) in paginatedProducts" :key="index" class="flex flex-col gap-3 items-center">
-                <router-link :to="`/details/${product.MaSanPham}`">
-                    <img :src="`${product.Images[0]}`"
-                        class="w-full [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]" alt="">
-                </router-link>
-                <router-link :to="`/details/${product.MaSanPham}`"
-                    class="text-white text-[14px] text-center flex-grow hover:text-[#DB3F4C] transition-all duration-300">{{
-                        product.TenSanPham }}</router-link>
-                <p class="text-white text-[14px]">Giá: <span class="text-[#FFD700]">{{ formatCurrency(product.GiaBan) }}
-                        VNĐ</span></p>
-                <button @click.prevent="addToCart(product.MaSanPham)" v-if="product.TrangThai === 'Đang bán'"
-                    class="px-5 py-2 w-full rounded-md bg-[#DB3F4C] text-white font-medium">Thêm giỏ hàng</button>
-                <button v-else-if="product.TrangThai === 'Ngừng kinh doanh'"
-                    class="px-5 py-2 w-full rounded-md bg-gray-600 text-white font-medium">Ngừng kinh doanh</button>
-                <button v-else-if="product.SoLuong <= 0"
-                    class="px-5 py-2 w-full rounded-md bg-gray-600 text-white font-medium">Hết hàng</button>
+        <div class="m-5 lg:mx-[210px]">
+            <div class="my-2 bg-gradient-to-b text-center lg:text-start from-[rgba(219,63,76,0.7)] to-[#1A1D27] p-5">
+                <p class="text-white font-semibold uppercase text-[22px] lg:text-[26px]">Vũ trụ Gundam thu nhỏ trong
+                    từng mô
+                    hình, chỉ có tại C3 GUNDAM.</p>
+                <p class="text-white font-medium my-1">Tại đây bạn có thể chọn được những mẫu Gundam yêu thích chính
+                    hãng từ
+                    BANDAI NAMCO.</p>
+            </div>
+            <div class="my-2 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+                <button v-for="(choice, index) in listChoices" :key="index"
+                    @click.prevent="selectTypeProducts(choice.type)"
+                    class="w-full bg-gradient-to-b text-center flex flex-col gap-2 lg:text-start from-[rgba(219,63,76,0.7)] to-[#1A1D27] p-4">
+                    <p class="text-[18px] font-semibold text-white">{{ choice.name }}</p>
+                    <span class="lg:w-[100px] w-full h-[2px] bg-white"></span>
+                    <p class="text-[14px] text-white font-medium">> Hiển thị tất cả</p>
+                </button>
+            </div>
+            <div class="w-full my-6 lg:grid grid-cols-4 gap-4 items-center">
+                <p class="text-[20px] font-semibold text-white hidden lg:block">Sắp xếp theo: </p>
+                <button v-for="item in arrange" :key="item" @click.prevent="selectTypeProducts(item.type)"
+                    class="border-2 hidden lg:block border-white text-white font-medium p-2 hover:text-[#DB3F4C] hover:border-[#DB3F4C] duration-300 transition-all">{{
+                    item.name }}</button>
+                <select v-model="selectedType" name="" id="" class="w-full p-4 font-semibold block lg:hidden">
+                    <option value="All">Sắp xếp theo</option>
+                    <option :value="item.type" v-for="item in arrange" :key="item">{{ item.name }}</option>
+                </select>
+            </div>
+            <div class="mb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+                <div v-for="(product, index) in paginatedProducts" :key="index"
+                    class="flex flex-col gap-3 items-center">
+                    <router-link :to="`/details/${product.MaSanPham}`">
+                        <img :src="`${product.Images[0]}`" class="w-full [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]"
+                            alt="">
+                    </router-link>
+                    <router-link :to="`/details/${product.MaSanPham}`"
+                        class="text-white text-[14px] text-center flex-grow hover:text-[#DB3F4C] transition-all duration-300">{{
+                            product.TenSanPham }}</router-link>
+                    <p class="text-white text-[14px]">Giá: <span class="text-[#FFD700]">{{
+                        formatCurrency(product.GiaBan) }}
+                            VNĐ</span></p>
+                    <button @click.prevent="addToCart(product.MaSanPham)" v-if="product.TrangThai === 'Đang bán'"
+                        class="px-5 py-2 w-full bg-[#DB3F4C] text-white font-medium">Thêm giỏ hàng</button>
+                    <button v-else-if="product.TrangThai === 'Ngừng kinh doanh'"
+                        class="px-5 py-2 w-full bg-gray-600 text-white font-medium">Ngừng kinh doanh</button>
+                    <button v-else-if="product.SoLuong <= 0"
+                        class="px-5 py-2 w-full bg-gray-600 text-white font-medium">Hết hàng</button>
+                </div>
             </div>
         </div>
         <div class="flex justify-center my-10 space-x-2">
-            <button v-for="page in totalPages" :key="page" 
-                @click="goToPage(page)" 
+            <button v-for="page in totalPages" :key="page" @click="goToPage(page)"
                 class="px-4 py-2 rounded-md border text-white cursor-pointer"
                 :class="page === currentPage ? 'bg-[#DB3F4C] border-[#DB3F4C]' : 'border-gray-500'">
                 {{ page }}
