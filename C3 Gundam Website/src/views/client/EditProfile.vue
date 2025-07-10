19145ad62fb01133e4b89f9f6b5aa64e75551792
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-07-10 20:52:29 +0700

    fix giao dien

--- C3 Gundam Website/src/views/client/EditProfile.vue
+++ C3 Gundam Website/src/views/client/EditProfile.vue
@@ -115,9 +115,9 @@ onMounted(() => {
 
 
 <template>
-    <div class="bg-gradient-to-br from-[#0F1419] via-[#1A1D27] to-[#0F1419] relative overflow-hidden min-h-screen font-sans scroll-smooth">
+    <div class="bg-gradient-to-br from-[#0F1419] via-[#1A1D27] to-[#0F1419] relative flex flex-col overflow-hidden min-h-screen font-sans scroll-smooth">
         <Header />
-        <div class="relative my-5 m-2 lg:mx-[200px] flex justify-center items-center">
+        <div class="relative my-5 m-2 lg:mx-[200px] flex justify-center items-center flex-grow">
             <div class="w-full m-4">
                 <div
                     class="bg-[#242424] overflow-hidden px-6 py-3 rounded [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]">
