2c7bce050d24b7dbde7062f34079e1f287713c9a
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-08-12 13:00:38 +0700

    fix ui

--- C3 Gundam Website/src/views/client/SearchResults.vue
+++ C3 Gundam Website/src/views/client/SearchResults.vue
@@ -175,7 +175,7 @@ onMounted(() => {
         </h1>
         <div v-if="listProducts.length > 0"
             class="mt-10 mb-20 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 m-5 lg:mx-[210px]">
-            <div v-for="(product, index) in paginatedProducts" :key="index" class="flex flex-col gap-3 items-center">
+            <div v-for="(product, index) in paginatedProducts" :key="index" class="flex flex-col gap-3 items-center group">
                 <router-link :to="`/details/${product.MaSanPham}`">
                     <img :src="`${product.Images[0]}`" class="w-full [box-shadow:0px_0px_6px_rgba(255,255,255,0.8)]"
                         alt="">
