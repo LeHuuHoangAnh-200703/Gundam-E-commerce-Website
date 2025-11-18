48c51cd3fccb515f397782970d5dc81340c12f59
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2025-11-18 08:44:28 +0700

    fix

--- Back_End/src/routes/Notifications.js
+++ Back_End/src/routes/Notifications.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const notificationController = require('../controllers/NotificationControllers');
+const notificationController = require('../controllers/notificationControllers');
 
 router.get("/", notificationController.getAllNotifications);
 router.get("/:maTB", notificationController.getNotification);
